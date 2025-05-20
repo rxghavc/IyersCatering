f9c11b1ec1f3c503c4828210df1ea8ad44f1ac55
rxghavc <r.commandur@gmail.com>
2025-05-20 19:22:46 +0100

    added route to homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
+import Home from './pages/Home';
 
 function App() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
   return (
-    <>
+    <Router>
       <Navbar onMenuClick={() => setSidebarOpen(true)} />
       <Sidebar open={sidebarOpen} onClose={() => setSidebarOpen(false)} />
-      {/* ...existing or future page content... */}
-    </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* Add more routes for About, Menu, Contact, etc. */}
+      </Routes>
+    </Router>
   );
 }
 
